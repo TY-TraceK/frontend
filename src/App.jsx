e6f559af2d376c6066e84d65f659ed70ef6da0c2
Dasom <ektha5032@gmail.com>
2026-09-13 01:21:14 +0900

    feat/place 공통 컴포넌트 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Header from "./components/Header";
+import GNB from "./components/GNB";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <GNB />
+    </>
   );
 }
+
 export default App;
---
+++ src/components/GNB.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import "./GNB.css";
+import {
+  ArchiveIcon,
+  HouseIcon,
+  MapTrifoldIcon,
+  StarIcon,
+  UserIcon,
+} from "@phosphor-icons/react";
+
+function GNB() {
+  return (
+    <nav>
+      <div className="container">
+        <section className="navigation ">
+          <Link to="/" className="menu home current">
+            <span className="icon">
+              <HouseIcon />
+            </span>
+            <span className="menu-name">홈</span>
+          </Link>
+          <Link className="menu contents">
+            <span className="icon">
+              <StarIcon />
+            </span>
+            <span className="menu-name">콘텐츠</span>
+          </Link>
+          <Link className="menu map">
+            <span className="icon">
+              <MapTrifoldIcon />
+            </span>
+            <span className="menu-name">지도</span>
+          </Link>
+          <Link className="menu archive">
+            <span className="icon">
+              <ArchiveIcon />
+            </span>
+            <span className="menu-name">아카이브</span>
+          </Link>
+          <Link className="menu profile">
+            <span className="icon">
+              <UserIcon />
+            </span>
+            <span className="menu-name">프로필</span>
+          </Link>
+        </section>
+      </div>
+    </nav>
+  );
+}
+
+export default GNB;
