a178c16364372ac5253451b6bed9e7f0056e8b31
yujin <yujin09219@naver.com>
2026-09-14 13:39:32 +0900

    fix: prettier 설정 파일 수정

--- src/prettier.config.js
+++ src/prettier.config.js
@@ -1,21 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import {defineConfig, globalIgnores} from "eslint/config";
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {ecmaFeatures: {jsx: true}},
-    },
-  },
-]);
+  semi: true,
+  singleQuote: true,
+
+  trailingComma: 'es5',
+
+  bracketSpacing: true,
+  bracketSameLine: false,
+
+  arrowParens: 'always',
+
+  endOfLine: 'lf',
+};
+
+export default config;
