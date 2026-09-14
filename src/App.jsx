e8eac16f4ee0ac90e8caf9a1be683a4167c5c8ca
yujin <yujin09219@naver.com>
2026-09-14 13:41:43 +0900

    feat: 로그인 및 콜백 페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,62 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import Place from "./pages/Place/Place";
+import ContentDetail from "./pages/content/ContentDetail";
 import Header from "./components/Header";
 import GNB from "./components/GNB";
-import ContentDetail from "./pages/content/ContentDetail";
 import VerifyFab from "./components/VerifyFab";
 import VerifyModal from "./components/VerifyModal";
+import { KakaoCallback, Login } from "@/pages/Login";
+import TokenStorage from "@/api/tokenStorage.js";
+
+function ProtectedRoute() {
+  const isLoggedIn = !!localStorage.getItem("accessToken");
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
 
 function App() {
   const [isVerifyOpen, setIsVerifyOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const isLoggedIn = !!TokenStorage.getAccessToken();
+
+  const handleVerifyClick = () => {
+    if (!isLoggedIn) {
+      navigate("/login");
+      return;
+    }
+
+    setIsVerifyOpen(true);
+  };
 
   return (
     <>
       <Header />
 
       <Routes>
+        {/* 로그인 없이 접근 가능 */}
         <Route path="/" element={<Home />} />
         <Route path="/place" element={<Place />} />
+
         <Route path="/content/detail" element={<ContentDetail />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
+
+        {/* 로그인 후 접근 가능한 페이지 */}
+        <Route element={<ProtectedRoute />}></Route>
       </Routes>
 
-      <VerifyFab onClick={() => setIsVerifyOpen(true)} />
+      <VerifyFab onClick={handleVerifyClick} />
 
       <VerifyModal
         isOpen={isVerifyOpen}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./styles/reset.css";
-import "./styles/tokens.css";
-import "./styles/common.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './styles/reset.css';
+import './styles/tokens.css';
+import './styles/common.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")).render(
+import './api/apiInterceptor';
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
