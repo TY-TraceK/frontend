faa0f4b098c2dd419a5f10bfa5cab6ea6f8a7931
Dasom <ektha5032@gmail.com>
2026-09-18 17:55:57 +0900

    [Refactor/UI layout] antd 커스텀 및 map, verify frame 추가 (#57)
    * ui-layout fix: map에 frame 추가
    
    * ui-layoust fix: verify modal, map frame 공통 스타일과 개별 스타일 적용 완료
    
    * ui-layout style: antd 커스텀 및 스크롤 기본 커스텀

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -41,132 +41,133 @@ function Map() {
   return (
     <main className="map">
       <div className="map-area"></div>
-
-      <div className="container">
-        <div
-          ref={chipsRef}
-          className="chips"
-          onPointerDown={handleChipPointerDown}
-          onPointerMove={handleChipPointerMove}
-          onPointerUp={handleChipPointerUp}
-          onPointerLeave={handleChipPointerUp}
-        >
-          {/* MEMO: 특정 칩 선택되면 button에 클래스 active 추가 바랍니다. */}
-          <button className="chip attraction active">
-            <span className="icon">
-              <SynagogueIcon />
-              <span className="category">관광지/명소</span>
-            </span>
-          </button>
-
-          <button className="chip culture">
-            <span className="icon">
-              <BankIcon />
-              <span className="category">문화시설</span>
-            </span>
-          </button>
-
-          <button className="chip event">
-            <span className="icon">
-              <ConfettiIcon />
-              <span className="category">축제/행사</span>
-            </span>
-          </button>
-
-          <button className="chip filming">
-            <span className="icon">
-              <FilmSlateIcon />
-              <span className="category">촬영지</span>
-            </span>
-          </button>
-
-          <button className="chip restaurant">
-            <span className="icon">
-              <ForkKnifeIcon />
-              <span className="category">음식점</span>
-            </span>
-          </button>
-
-          <button className="chip cafe">
-            <span className="icon">
-              <CoffeeIcon />
-              <span className="category">카페</span>
-            </span>
-          </button>
-
-          <button className="chip accommodation">
-            <span className="icon">
-              <BedIcon />
-              <span className="category">숙박</span>
-            </span>
-          </button>
-
-          <button className="chip shopping">
-            <span className="icon">
-              <ShoppingBagOpenIcon />
-              <span className="category">쇼핑</span>
-            </span>
-          </button>
-
-          <button className="chip other">
-            <span className="icon">
-              <AsteriskIcon />
-              <span className="category">기타</span>
-            </span>
-          </button>
-        </div>
-      </div>
-
-      <button className="floating bookmark icon">
-        <BookmarkSimpleIcon />
-      </button>
-
-      <section className="place-sheet">
-        <div className="place-list">
-          <button className="sheet-handle" aria-label="바텀시트 조절">
-            <span></span>
-          </button>
-
-          <article className="item">
-            <div className="image">
-              <img src="https://picsum.photos/id/10/600/400" alt="" />
-            </div>
-
-            <div className="place-info">
-              <div className="meta">
+      <div className="frame">
+        <div className="container">
+          <div
+            ref={chipsRef}
+            className="chips"
+            onPointerDown={handleChipPointerDown}
+            onPointerMove={handleChipPointerMove}
+            onPointerUp={handleChipPointerUp}
+            onPointerLeave={handleChipPointerUp}
+          >
+            {/* MEMO: 특정 칩 선택되면 button에 클래스 active 추가 바랍니다. */}
+            <button className="chip attraction active">
+              <span className="icon">
+                <SynagogueIcon />
                 <span className="category">관광지/명소</span>
-                <span className="verify-count accent-text">
-                  방문 인증 1,279 건
-                </span>
-              </div>
+              </span>
+            </button>
 
-              <h2>송도해수욕장</h2>
+            <button className="chip culture">
+              <span className="icon">
+                <BankIcon />
+                <span className="category">문화시설</span>
+              </span>
+            </button>
 
-              <div className="business-hours-info">
-                <span className="icon">
-                  <ClockIcon weight="fill" />
-                </span>
-                <p className="info">
-                  <span className="start-time">06:00</span>
-                  <span className="close-time">23:00</span>
-                  <span className="additional">연중무휴</span>
-                </p>
-              </div>
+            <button className="chip event">
+              <span className="icon">
+                <ConfettiIcon />
+                <span className="category">축제/행사</span>
+              </span>
+            </button>
 
-              <div className="address-info">
-                <span className="icon">
-                  <MapPinIcon weight="fill" />
-                </span>
-                <p className="info">
-                  <span className="address">
-                    부산광역시 서구 송도해변로 100
-                  </span>
-                </p>
-              </div>
-            </div>
-          </article>
+            <button className="chip filming">
+              <span className="icon">
+                <FilmSlateIcon />
+                <span className="category">촬영지</span>
+              </span>
+            </button>
+
+            <button className="chip restaurant">
+              <span className="icon">
+                <ForkKnifeIcon />
+                <span className="category">음식점</span>
+              </span>
+            </button>
+
+            <button className="chip cafe">
+              <span className="icon">
+                <CoffeeIcon />
+                <span className="category">카페</span>
+              </span>
+            </button>
+
+            <button className="chip accommodation">
+              <span className="icon">
+                <BedIcon />
+                <span className="category">숙박</span>
+              </span>
+            </button>
+
+            <button className="chip shopping">
+              <span className="icon">
+                <ShoppingBagOpenIcon />
+                <span className="category">쇼핑</span>
+              </span>
+            </button>
+
+            <button className="chip other">
+              <span className="icon">
+                <AsteriskIcon />
+                <span className="category">기타</span>
+              </span>
+            </button>
+          </div>
         </div>
-      </section>
+
+        <button className="floating bookmark icon">
+          <BookmarkSimpleIcon />
+        </button>
+
+        <section className="place-sheet">
+          <div className="place-list">
+            <button className="sheet-handle" aria-label="바텀시트 조절">
+              <span></span>
+            </button>
+
+            <article className="item">
+              <div className="image">
+                <img src="https://picsum.photos/id/10/600/400" alt="" />
+              </div>
+
+              <div className="place-info">
+                <div className="meta">
+                  <span className="category">관광지/명소</span>
+                  <span className="verify-count accent-text">
+                    방문 인증 1,279 건
+                  </span>
+                </div>
+
+                <h2>송도해수욕장</h2>
+
+                <div className="business-hours-info">
+                  <span className="icon">
+                    <ClockIcon weight="fill" />
+                  </span>
+                  <p className="info">
+                    <span className="start-time">06:00</span>
+                    <span className="close-time">23:00</span>
+                    <span className="additional">연중무휴</span>
+                  </p>
+                </div>
+
+                <div className="address-info">
+                  <span className="icon">
+                    <MapPinIcon weight="fill" />
+                  </span>
+                  <p className="info">
+                    <span className="address">
+                      부산광역시 서구 송도해변로 100
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
